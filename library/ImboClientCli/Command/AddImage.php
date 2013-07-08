d2aa07da962fd2f9fd6c54babb8796325d1fa57a
Mats Lindh <mats@lindh.no>
2013-07-08 22:44:51 +0200

    Only ask for confirmation before uploading after scanning a directory structure.

--- library/ImboClientCli/Command/AddImage.php
+++ library/ImboClientCli/Command/AddImage.php
@@ -52,7 +52,9 @@ class AddImage extends RemoteCommand {
             throw new InvalidArgumentException('The specified path does not exist: ' . $path);
         }
 
-        if (is_file($fullPath)) {
+        $singleFile = is_file($fullPath);
+
+        if ($singleFile) {
             $files = array($fullPath);
         } else {
             $suffixes = $input->getOption('suffixes');
@@ -79,11 +81,7 @@ class AddImage extends RemoteCommand {
         }
 
         $dialog = $this->getHelper('dialog');
-        $result = true;
-
-        if (count($files) > 5) {
-            $result = $dialog->askConfirmation($output, 'You are about to add ' . count($files) . ' images to "' . $this->server['name'] . '". Continue? [yN] ', false);
-        }
+        $result = $singleFile ?: $dialog->askConfirmation($output, 'You are about to add ' . count($files) . ' images to "' . $this->server['name'] . '". Continue? [yN] ', false);
 
         if ($result) {
             $client = new ImboClient($this->server['url'], $this->server['publicKey'], $this->server['privateKey']);
