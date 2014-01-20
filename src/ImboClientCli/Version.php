44380ec6b782455a610af64aa0ee2c5cc7f5da0d
Christer Edvartsen <cogo@starzinger.net>
2014-01-20 19:44:06 +0100

    Remove PEAR-related stuff and fixed some comments

--- src/ImboClientCli/Version.php
+++ src/ImboClientCli/Version.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of the Imbo package
+ * This file is part of the ImboClientCli package
  *
  * (c) Christer Edvartsen <cogo@starzinger.net>
  *
@@ -20,31 +20,7 @@ class Version {
     /**
      * The current version
      *
-     * This string will be replaced by the actual version when installed via pear
-     *
      * @var string
      */
-    static private $id = '@package_version@';
-
-    /**
-     * Get the version number only
-     *
-     * @return string
-     */
-    static public function getVersionNumber() {
-        if (strpos(self::$id, '@package_version') === 0) {
-            return 'dev';
-        }
-
-        return self::$id;
-    }
-
-    /**
-     * Get the version string
-     *
-     * @return string
-     */
-    static public function getVersionString() {
-        return 'ImboClientCli-' . self::getVersionNumber() . ' by Christer Edvartsen';
-    }
+    const VERSION = 'dev';
 }
